c2a9ca84ee718d0b13d427eec5a10296d525920e
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-05-24 16:54:29 +0200

    layout 2.0

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,17 @@
 @section('content')
     <section>
         <div class="container py-5 my-5">
-            <div class="row row-cols-5 g-5">
+            <div class="row row-cols-5 g-3">
                 @forelse ($movies as $movie)
-                    <div class="col-auto">
+                    <div class="col">
                         <div class="card">
-                            <div class="card-header">
+                            <div class="card-header p-0">
                                 <img src="{{ $movie->path_img }}" alt="" class="card-img-top">
                             </div>
                             <div class="card-body text-center">
-                                <h3 class="crad-title">
+                                <h5 class="crad-title">
                                     {{ $movie->title }}
-                                </h3>
+                                </h5>
                                 <p class="card-text">
                                     {{ $movie->overview }}
                                 </p>

84d1536783d4f873cc87465cdc60aab3c8d17eb9
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-05-24 16:14:59 +0200

    add pseudo layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,31 @@
 @extends('layouts.app')
+
+@section('content')
+    <section>
+        <div class="container">
+            <div class="row row-cols-5 gap-3">
+                @forelse ($movies as $movie)
+                    <div class="col-auto">
+                        <div class="card">
+                            <img src="" alt="" class="card-img-top">
+                            <div class="card-body">
+                                <h4>
+                                    {{ $movie->title }}
+                                </h4>
+                                <p>
+                                    {{ $movie->vote }}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                    <div class="col">
+                        <p>
+                            No movies found 🤷‍♂️
+                        </p>
+                    </div>
+                @endforelse
+            </div>
+        </div>
+    </section>
+@endsection
